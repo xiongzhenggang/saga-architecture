c0b857d817b0a629c3667da27495d535c14a2eb6
xiongzhenggang <ab1290014555@qq.com>
2023-11-05 22:28:10 +0800

    saga init

---
+++ saga-architecture-account/src/main/java/com/xzg/sagaarchitectureaccount/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.xzg.sagaarchitectureaccount.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/test")
+public class TestController {
+
+    @GetMapping("/test")
+    public String getName(){
+        return "xx";
+    }
+}
---
+++ saga-architecture-account/src/test/java/com/xzg/sagaarchitectureaccount/SagaArchitectureAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzg.sagaarchitectureaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaArchitectureAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
