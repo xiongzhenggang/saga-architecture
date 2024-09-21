b9d4fa67e0971d53cd98c3d1b2bfa9ea2b63ff34
xiongzhenggang <ab1290014555@qq.com>
2024-09-21 11:02:39 +0800

    gitlab and jenkins

---
+++ saga-architecture-orchestrator/saga-architecture-orchestrator-kit/src/main/java/com/xzg/orchestrator/kit/orchestration/saga/SagaTransactionMsgService.java
@@ -0,0 +1,17 @@
+package com.xzg.orchestrator.kit.orchestration.saga;
+
+/**
+ * @projectName: saga-architecture
+ * @package: com.xzg.orchestrator.kit.orchestration.saga
+ * @className: SagaTransationMsgService
+ * @author: xzg
+ * @description: TODO
+ * @date: 12/8/2024-下午 8:57
+ * @version: 1.0
+ */
+public class SagaTransactionMsgService {
+
+}
+
+
+    
